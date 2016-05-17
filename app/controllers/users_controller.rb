166280e86c81ebf9002dbe93bc5ab6ea6376dbd5
ratovia <ratovia@hotmail.co.jp>
2016-05-18 03:20:52 +0900

    flash

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -9,6 +9,8 @@ class UsersController < ApplicationController
   def create
     @user = User.new(user_params)
     if @user.save
+      flash[:success] = "Welcome to the wine app!"
+      redirect_to @user
     else
       render 'new'
     end
