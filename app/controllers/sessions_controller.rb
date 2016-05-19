aebbdd965293ff5dfd8697d96b6f7b6015de6781
ratovia <ratovia@hotmail.co.jp>
2016-05-19 18:53:51 +0900

    login user

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -6,9 +6,11 @@ class SessionsController < ApplicationController
     user = User.find_by(email: params[:session][:email].downcase)
     if user && user.authenticate(params[:session][:password])
       # ユーザーログイン後にユーザー情報のページにリダイレクトする
+      login user
+      redirect_to user
     else
       # エラーメッセージを作成する
-      flash.now[:danger] = 'Invalid email/password combination' 
+      flash.now[:danger] = 'Invalid email/password combination'
       render 'new'
     end
   end
