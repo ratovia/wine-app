691b2f895cd785fed63854fb0bfd988b5b59b467
ratovia <ratovia@hotmail.co.jp>
2016-05-19 19:18:05 +0900

    login method

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -6,7 +6,7 @@ class SessionsController < ApplicationController
     user = User.find_by(email: params[:session][:email].downcase)
     if user && user.authenticate(params[:session][:password])
       # ユーザーログイン後にユーザー情報のページにリダイレクトする
-      login user
+      log_in user
       redirect_to user
     else
       # エラーメッセージを作成する
