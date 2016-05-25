54f1f4ffd463e41d97c97fad4eba238b9ffccc7b
ratovia <ratovia@hotmail.co.jp>
2016-05-25 18:20:10 +0900

    index user show all

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,8 +1,12 @@
 class UsersController < ApplicationController
-  before_action :logged_in_user,only:[:edit,:update]
+  before_action :logged_in_user,only:[:index,:edit,:update]
   before_action :current_user, only:[:edit,:update]
   def show
     @user = User.find(params[:id])
+  end
+
+  def index
+    @users = User.all
   end
 
   def new
