36c59370fa1094d0bb3a9c25d9e67fc1681c1124
ratovia <ratovia@hotmail.co.jp>
2016-05-26 03:22:45 +0900

    delete user

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,7 @@
 class UsersController < ApplicationController
-  before_action :logged_in_user,only:[:index,:edit,:update]
+  before_action :logged_in_user,only:[:index,:edit,:update,:destroy]
   before_action :current_user, only:[:edit,:update]
+  before_action :admin_user,     only: :destroy
   def show
     @user = User.find(params[:id])
   end
@@ -22,6 +23,12 @@ class UsersController < ApplicationController
     else
       render 'new'
     end
+  end
+
+  def destroy
+    User.find(params[:id]).destroy
+    flash[:success] = "User deleted"
+    redirect_to users_url
   end
 
   def edit
@@ -61,5 +68,9 @@ class UsersController < ApplicationController
     def correct_user
       @user = User.find(params[:id])
       redirect_to(root_url) unless current_user?(@user)
+    end
+
+    def admin_user
+      redirect_to(root_url) unless current_user.admin?
     end
 end
