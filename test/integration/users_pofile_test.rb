4ee8fa23822777ec6db34e5e66d573616d16aee0
ratovia <ratovia@hotmail.co.jp>
2016-05-31 06:07:52 +0900

    access valid

---
+++ test/integration/users_pofile_test.rb
@@ -0,0 +1,22 @@
+require 'test_helper'
+
+class UsersPofileTest < ActionDispatch::IntegrationTest
+  include ApplicationHelper
+
+  def setup
+    @user = users(:michael)
+  end
+
+  test "profile display" do
+    get user_path(@user)
+    assert_template 'users/show'
+    assert_select 'title' , full_title(@user.name)
+    assert_select 'h1', text: @user.name
+    assert_select 'h1>img.gravatar'
+    assert_match @user.posts.count.to_s,response.body
+    assert_select 'div.pagination'
+    @user.posts.paginate(page: 1).each do |post|
+      assert_match post.wine,response.body
+    end
+  end
+end
