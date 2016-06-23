a504c572cb656752a1a48450ba10232e67eec91a
ratovia <ratovia@hotmail.co.jp>
2016-06-23 14:52:57 +0900

    6/23

---
+++ app/assets/javascripts/post.js
@@ -0,0 +1,17 @@
+// $('#post_picture').bind('change', function(){
+//   var size_in_megabytes = this.files[0].size/1024/1024;
+//   if(size_in_megabytes > 5){
+//     alert('file > 5MB error');
+//   }
+// })
+//
+// $("#sweet").slider({
+//   ticks: [0, 100, 200, 300, 400],
+//   ticks_labels: ['甘い', '', '', '', '辛い'],
+//   ticks_snap_bounds: 30
+// });
+// $("#depth").slider({
+//   ticks: [0, 100, 200, 300, 400],
+//   ticks_labels: ['スッキリ', '', '', '', '濃い'],
+//   ticks_snap_bounds: 30
+// });
